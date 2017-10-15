95ac942a121dccd1618d329bc840617f4e51ffa2
Jenny Chen <jenny7770002003@gmail.com>
2017-10-15 16:30:28 +0800

    加上可以設定副本狀態

--- js/login.js
+++ js/login.js
@@ -21,7 +21,6 @@ function login() {
   } else {
     console.log("start get data");
     console.log("user is = " + userId + " password is = " + password + ", start login");
-    console.log("db = " + firestoreDB.collection("member"));
     loginWithRealtimeDB(userId, password);
   }
 }
@@ -45,34 +44,6 @@ function loginWithRealtimeDB(userId, password) {
       $("#form").submit();
     }
   });
-}
-
-function loginWithFirestore() {
-  firestoreDB = firebase.firestore();
-  firestoreDB.collection("member").doc(userId).get().then(function(doc) {
-      console.log(userId + "是否存在" + doc.exists);
-
-      if (!doc.exists) {
-        $("#form_hint_text").text("此帳號不存在");
-
-      } else if (doc.get("password") != password) {
-        $("#form_hint_text").text("密碼錯誤");
-
-      } else {
-        console.log("login success, set cookie");
-        //set cookie here
-        Cookies.set('user_id', userId);
-        Cookies.set('user_nick_name', doc.get("nick_name"));
-        Cookies.set('user_is_admin', ("" + doc.get("is_admin")));
-
-        console.log("successully set cookie, submit form");
-        $("#form").submit();
-      }
-    })
-    .catch(function(error) {
-      console.log("Error getting document:" + error);
-      $("#form_hint_text").text("發生錯誤，請重新再試");
-    });
 }
 
 function checkLogin() {
